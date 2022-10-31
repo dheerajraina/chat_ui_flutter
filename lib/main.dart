f77f8af3e461850f55c37c6f798d0a0940d64e1b
Dheeraj <dheerajraina55@gmail.com>
2022-10-31 12:01:23 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:chat_ui_flutter/pages/chat/views/views.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blue,
+      ),
+      home: const ChatPage(),
+    );
+  }
+}
+
---
+++ lib/pages/chat/views/chat_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Container(child: Center(child:Text("Chat Page")),)
+    );
+  }
+}
